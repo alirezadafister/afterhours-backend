8e318cddf184a41bcbb50d65d2e8c3ccf78611cf
alirezadafister <alirezadarayandeh2005@gmail.com>
2026-09-25 14:38:16 +0330

    Allow CORS from live Vercel site

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://after-hour-wine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
